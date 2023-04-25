1a05a72652542d4e98bc43076b5978db83465280
irenetjrs <irkapidgorodetska@gmail.com>
2023-04-25 16:07:52 +0300

    done

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-import express from 'express'
+import mongoose from 'mongoose';
+import express from 'express';
+import { Product } from './model/product.js';
 
 
 const PORT = 3000;
 
+const url = 'mongodb://localhost:27017/shop';
+
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send('<h1>Wellcome</h1>');
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log('Connected to DB');
+    app.listen(PORT, () => {
+      console.log(`Server started on http://localhost:${PORT}`);
+    });
+app.get('/', (req, res) => {
+    Product.find()
+        .then(products => {
+        const productsHtml = products.map(product => `
+<div style="border: 1px solid #000; 
+width: fit-content; 
+margin: 0 0 20px 0; 
+padding: 0 10px">
+  <h2>${product.title}</h2>
+  <p>Price: ${product.price}</p>
+</div>
+        `);
+        const html = `<h1>Users purchases:</h1> ${productsHtml.join('')}`;
+        res.send(html);
+        })
+        .catch(error => {
+        console.error(error);
+        });
+    });
 })
-
-app.listen(PORT, ()=> {
-    console.log(`Server started on http://localhost:${PORT}`);
-})
+.catch((err) => {
+  console.log(`DB connection error: ${err}`);
+});
